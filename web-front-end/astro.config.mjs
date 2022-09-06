1aad7bf9630230defaae6537153932300869ae2e
AustinPena <austinpena@taloflow.ai>
2022-09-06 09:26:12 -0400

    added more info for n grams

--- web-front-end/astro.config.mjs
+++ web-front-end/astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +11,5 @@ export default defineConfig({
     config: {
       applyBaseStyles: false
     }
-  }), react(), mdx()]
+  }), react(), mdx(), image()]
 });
