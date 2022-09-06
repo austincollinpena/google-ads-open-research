eb4343a2c436c14ae78a8bada0577f33cd46ec03
AustinPena <austinpena@taloflow.ai>
2022-09-06 11:16:04 -0400

    update images

--- web-front-end/astro.config.mjs
+++ web-front-end/astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
@@ -7,9 +7,14 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), react(), mdx(), image()]
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    react(),
+    mdx(),
+    image(),
+  ],
 });
