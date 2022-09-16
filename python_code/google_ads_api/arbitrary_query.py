911f6180d1f36a8e6ed759ab162d2a14940ad72f
AustinPena <austinpena@taloflow.ai>
2022-09-16 09:59:00 -0700

    find incorrect negatives

--- python_code/google_ads_api/arbitrary_query.py
+++ python_code/google_ads_api/arbitrary_query.py
@@ -3,6 +3,8 @@ from typing import Optional
 from google_ads_api.make_client import make_client
 import os
 from dotenv import dotenv_values
+import hashlib
+from os.path import exists
 
 
 def create_where_string(where_argument: Optional[str]) -> Optional[str]:
@@ -18,7 +20,7 @@ def create_order_by_string(order_by: Optional[str]) -> Optional[str]:
 
 
 def arbitrary_google_ads_query(account_id: str, mcc_id: Optional[str], select: list[str], from_arg: str, where_argument: Optional[str],
-                               order_by: Optional[str]) -> Optional[pd.DataFrame]:
+                               order_by: Optional[str], parquet_cache=True) -> Optional[pd.DataFrame]:
     """
     :param account_id: the ID of the Google account you're requesting. Format 12345678
     :param mcc_id: if you only have access to this account through your MCC, pass through your MCC ID
@@ -26,6 +28,7 @@ def arbitrary_google_ads_query(account_id: str, mcc_id: Optional[str], select: l
     :param from_arg: List of resources found here: https://developers.google.com/google-ads/api/reference/rpc/v11/Campaign
     :param where_argument: Filter your query. Pass through a condition like "metrics.clicks > 2"
     :param order_by: Order your query. Pass through an argument like "metrics.clicks DESC"
+    :param parquet_cache: choose if parquet cache works
     :return: returns a dataframe with your "select" terms as the column names
     """
 
@@ -34,6 +37,12 @@ def arbitrary_google_ads_query(account_id: str, mcc_id: Optional[str], select: l
             f'FROM {from_arg} ' + \
             f'{create_where_string(where_argument)} ' + \
             f'{create_order_by_string(order_by)} '
+
+    if parquet_cache:
+        hashed_query = hashlib.sha256(query.encode('utf-8')).hexdigest()
+        file_exists = exists(f"./google_ads_api/git_ignored_data/{hashed_query}")
+        if file_exists:
+            return pd.read_parquet(f"./google_ads_api/git_ignored_data/{hashed_query}")
 
     client = make_client(mcc_id)
     ga_service = client.get_service("GoogleAdsService")
@@ -56,18 +65,33 @@ def arbitrary_google_ads_query(account_id: str, mcc_id: Optional[str], select: l
                 if criteria == "segments.device":
                     print(row)
                 # the row is not subscriptable so we need to use getattr for dynamic queries
-                first_level_key = criteria.split(".")[0]
-                second_level_key = criteria.split(".")[1]
-                row_attribute = getattr(row, first_level_key)
-                value_attribute = getattr(row_attribute, second_level_key)
-                if hasattr(value_attribute, 'name'):
-                    value_attribute = getattr(value_attribute, 'name')
+                # (we can't just say go get row[campaign_criterion.criterion_id]
+                # this needs to handle getting the GRPC attribute from a row for items like
+                # campaign_criterion.criterion_id or campaign_criterion.keyword.text
+                # Because of that, I split the keys up into parts and return the final value
+                # which should be the scalar we're looking for
+                split_keys = criteria.split(".")
+                value = None
+                attribute = None
+                for key in split_keys:
+                    if attribute is None:
+                        attribute = getattr(row, key)
+                    else:
+                        attribute = getattr(attribute, key)
+                    value = attribute
 
-                result[criteria] = value_attribute
+                if hasattr(value, 'name'):
+                    value = getattr(value, 'name')
+
+                result[criteria] = value
             result_dict.append(result)
 
     if not result_dict:
         return None
+
+    if parquet_cache:
+        df = pd.DataFrame(result_dict)
+        df.to_parquet(f"./google_ads_api/git_ignored_data/{hashed_query}")
 
     return pd.DataFrame(result_dict)
 
